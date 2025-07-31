44395f8d9738bbb82ab4a15d61235630f12d572f
lizto <lie449@gmail.com>
2025-07-30 21:19:07 -0500

    se crea en el main la ruta de origen del nltk

---
+++ main.py
@@ -0,0 +1,7 @@
+# Importar la libreria nltk
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+#definir la ruta donde se almacenaran los datos descargados de ntlk
+nltk.data.path.append('C:\Users\IDEAP3 R5\AppData\Roaming\nltk_data')
